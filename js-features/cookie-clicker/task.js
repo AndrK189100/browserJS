e58c9d144cef82b898d8b2bcc9838f3d2af32d77
AndrK189100 <andrka@gmail.com>
2022-11-17 14:15:26 +0300

    commit

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,25 @@
+
+const cookie = document.getElementById('cookie');
+const clickerCounter = document.getElementById('clicker__counter');
+const clickerSpeed = document.getElementById('clicker__speed');
+
+let flag = true;
+let lastClick = 0;
+let currClick = 0;;
+cookie.onclick = () => {
+    clickerCounter.textContent = Number(clickerCounter.textContent) + 1;
+    currClick = Date.now();
+    clickerSpeed.textContent = (1 / ((currClick - lastClick) / 1000)).toFixed(3);
+    lastClick = currClick;
+    if (flag) {
+        cookie.width += 20;
+        flag = false;
+        
+    }
+    else {
+        cookie.width -=20;
+        flag = true;
+        
+    }
+
+}
