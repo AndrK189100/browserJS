a25f4586e927a74b458ca100ce21236fbfe25b08
AndrK189100 <andrka@gmail.com>
2022-11-21 15:02:05 +0300

    commit

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,18 +1,7 @@
-document.body.querySelector('.dropdown__value').addEventListener('click', dropDown);
+document.body.querySelector('.dropdown__value').addEventListener('click', 
+                                () => document.body.querySelector('.dropdown__list').classList.toggle('dropdown__list_active'));
 
 Array.from(document.body.querySelectorAll('.dropdown__link')).forEach( item => {item.addEventListener('click', selectItem)});
-
-
-function dropDown() {
-    if(document.body.querySelector('.dropdown__list').classList.contains('dropdown__list_active')) {
-        document.body.querySelector('.dropdown__list').classList.remove('dropdown__list_active');
-    }
-    else {
-        document.body.querySelector('.dropdown__list').classList.add('dropdown__list_active');
-    }
-        
-
-}
 
 function selectItem(event) {
     document.body.querySelector('.dropdown__list').classList.remove('dropdown__list_active');
