d3833fd569725489ea2d697c8eeb0ff8de4406f3
AndrK189100 <andrka@gmail.com>
2022-11-29 11:17:14 +0300

    commit

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -31,9 +31,11 @@ function productAdd(env) {
     const product = this.closest('.product');
     const product_id = product.dataset.id;
     const product_quantity = product.querySelector('.product__quantity-value').textContent;
+    const product_image = product.querySelector('.product__image').src;
 
     Array.from(cart.querySelectorAll('.cart__product')).forEach( cartProduct => {
         if (cartProduct.dataset.id === product_id) {
+            productMove(product, product_image, cartProduct);
             cartProduct.querySelector('.cart__product-count').textContent = 
                 Number(cartProduct.querySelector('.cart__product-count').textContent) + Number(product_quantity);
             isCart = true;    
@@ -43,7 +45,7 @@ function productAdd(env) {
 
     if(isCart) return;
     
-    const product_image = product.querySelector('.product__image').src;
+    
     const cartProduct = document.createElement('div');
     cartProduct.classList.add('cart__product');
     cartProduct.setAttribute('data-id', product_id);
@@ -52,14 +54,55 @@ function productAdd(env) {
                       <div class="cart__product-count">${product_quantity}</div>
                       <div class="cart__product-delete">X</div>`;
     
-    cartProduct.addEventListener('click', productRemove);                  
+    cartProduct.querySelector('.cart__product-delete').addEventListener('click', productRemove);                  
 
     cart.append(cartProduct);
-    
+
     document.body.querySelector('.cart').style['display'] = 'block';
 
-    true;
+    productMove(product, product_image, cartProduct);
 
+}
+
+function productMove(product, product_image, cartProduct) {
+    
+    const coordinateProduct = product.querySelector('.product__image').getBoundingClientRect();
+    const coordinateCartProduct = cartProduct.getBoundingClientRect();
+
+    const yWay = coordinateProduct.top -coordinateCartProduct.top;
+    const xWay = coordinateCartProduct.left;
+    const yStep = yWay / 20;
+    const xStep = xWay / 20;
+
+    const img = document.createElement('div');
+    img.classList.add('product-move');
+    img.innerHTML = `<img class="cart__product-image" src=${product_image}>`;
+    img.style['top'] = coordinateProduct.top + 'px';
+    img.style['left'] = coordinateProduct.left + 'px';
+
+    let imgY = coordinateProduct.top;
+    let imgX = coordinateProduct.left
+
+    document.body.appendChild(img);
+    
+    const timer = ms => new Promise(res => setTimeout(res, ms))
+    
+    async function f() {
+    for(let i = 0; i < 20; i++) {
+    
+        imgY -= yStep;
+        imgX +=xStep;
+
+        img.style['top'] = imgY + 'px';
+        img.style['left'] = imgX + 'px';
+        await timer(1);
+    }
+    
+    img.style['display'] = 'none';
+    cartProduct.style['visibility'] = 'visible';
+    }
+    
+    f();
 }
 
 function productRemove(env) {
